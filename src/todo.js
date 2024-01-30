5c3f2eff33a8ab208da700dddb1f7fccf1c12035
Praise <praisem52@gmail.com>
2024-01-29 14:12:52 -1100

    Cancel Button added to projects dialog

--- src/todo.js
+++ src/todo.js
@@ -2,6 +2,7 @@
 
 const addProject=document.querySelector("#addProject button");
 let myProjects=[];
+let myTasks=[];
 let project;
 
 function projDialog(){
@@ -12,12 +13,14 @@ function projDialog(){
                             <input type="text" id="project" placeholder="Project's Name"/>
                             <div>
                                 <button type="button" id="closeProj" class="button">Close</button>
+                                <button type="button" id="cancel" class=button>Cancel</button>
                             </div>
                         </form>
     `;
     let projectName=dialogProject.querySelector("#project");
     let closeProjectButton=dialogProject.querySelector("#closeProj");
-   return {dialogProject,projectName,closeProjectButton}
+    let cancelButton=dialogProject.querySelector("#cancel");
+    return {dialogProject,projectName,closeProjectButton,cancelButton}
 }
 addProject.addEventListener("click",()=>{
     project=projDialog();
@@ -30,9 +33,16 @@ addProject.addEventListener("click",()=>{
         document.body.removeChild(project.dialogProject)
     })
     project.dialogProject.addEventListener("close",()=>{
-        myProjects.push(project.projectName.value);
-        console.log(displayProject(myProjects))
-        console.log(myProjects);
+        if(project.projectName.value==true){
+            myProjects.push(project.projectName.value);
+            console.log(displayProject(myProjects))
+            console.log(myProjects);
+        }
+    })
+    project.cancelButton.addEventListener("click",()=>{
+        project.projectName.value="";
+        project.dialogProject.close();
+        document.body.removeChild(project.dialogProject);
     })
 })
 
@@ -43,4 +53,34 @@ function displayProject(works=[]){
                         </div>`
     let x=projects.getElementsByClassName("buttonProjects");
     return x
+}
+
+function createTaskDialog(){
+    const dialogTask=document.createElement("dialog");
+    
+    dialogTask.innerHTML=`<form>
+                            <div>
+                                <input type="text" id="title" placeholder="Title"/>
+                            </div>
+                            <div>
+                                <textarea type="text" id="description" placeholder="description"/>
+                            </div>
+                            <div>
+                                <label for="date">Due Date</label>
+                                <input type="date" id="date"/>
+                            </div>
+                            <div>
+                                <label for="priority">Priority</label>
+                                <select id="priority">
+                                    <option>High</option>
+                                    <option>Medium</option>
+                                    <option>Low</option>
+                                </select>
+                            </div>
+                            <div>
+                                <button>Add</button>
+                                <button>Cancel</button>
+
+                          </form>`
+    return dialogTask
 }
